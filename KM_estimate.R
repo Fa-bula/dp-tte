f79fdb78c6a41cf863222aab928c1d0a26209857
fa-bula <fa_bula@mail.ru>
2024-09-01 18:04:58 +0200

    Added hazard_ratio, logrank_pvalue, renamed cnsr to event

--- KM_estimate.R
+++ KM_estimate.R
@@ -2,10 +2,33 @@
 library(survival)
 
 # Function to calculate KM estimate at a specific time point
-calculate_km_estimate <- function(df, timepoint) {
+calculate_km_estimate <- function(df, timepoint=1) {
   # Fit the Kaplan-Meier survival curve
-  fit <- survfit(Surv(event_times, cnsr) ~ 1, data = df)
+  fit <- survfit(Surv(event_times, event) ~ 1, data = df)
   
   # Calculate the Kaplan-Meier estimate at specific timepoint
   return(summary(fit, times = timepoint)$surv)
+}
+
+calculate_hazard_ratio <- function(df, timepoint=1) {
+  # Create a survival object
+  surv_obj <- Surv(time = df$event_times, event = df$event)
+  # Fit Cox proportional hazards model
+  cox_model <- coxph(formula = surv_obj ~ trt, data = df)
+  # Extract hazard ratio
+  hazard_ratio <- exp(coef(cox_model))
+  return(hazard_ratio)
+}
+
+calculate_logrank_pvalue <- function(df, timepoint=1) {
+  # Performing Log-rank test on df
+  diff <- survdiff(Surv(
+    time = event_times,
+    event = event,
+    type = 'right'
+  ) ~ trt,
+  data = df)
+  
+  logrank_pval = pchisq(diff$chisq, length(diff$n) - 1, lower.tail = FALSE)
+  return(logrank_pval)
 }
--- find_global_sensitivity.R
+++ find_global_sensitivity.R
@@ -3,20 +3,35 @@ source("KM_estimate.R")
 # Iterates over all possible time-to-event data sets for num_trt treatment groups
 # with N subjects in each treatment group
 find_global_sensitivity <- function(num_trt, N, max_aval, f) {
-  all_vectors <- expand.grid(replicate(num_trt * N, 0:max_aval, simplify = FALSE))
+  all_vectors <- expand.grid(replicate(num_trt * N, 1:max_aval, simplify = FALSE))
   differences <- sapply(1:nrow(all_vectors), function(i) {
     # Construct data set df
     event_times <- unlist(all_vectors[i, ])
-    cnsr <- rep(1, N * num_trt)
+    event <- rep(1, N * num_trt)
     trt <- rep(1:num_trt, each = N)
-    df <- data.frame(event_times, cnsr, trt)
-    f_value <- f(df, 1)
-    f_value_without_first <- f(df[-1, ], 1)
-    difference <- f_value - f_value_without_first
-    return(difference)
+    df <- data.frame(event_times, event, trt)
+    result <- tryCatch(
+      {
+        f_value <- f(df, 1)
+        f_value_without_first <- f(df[-1, ], 1)
+        difference <- abs(f_value - f_value_without_first)
+        return(difference)
+      },
+      error = function(e) {
+        cat("An error occurred: ", conditionMessage(e), "\n")
+        # Perform additional error handling or return a default value
+        return(NA)
+      }
+    )
   })
   return(max(as.numeric(differences), na.rm = TRUE))
 }
 
-cat(paste("Global sensitivity of", deparse(substitute(calculate_km_estimate)),
-            find_global_sensitivity(1, 7, 2, calculate_km_estimate)))
+# cat(paste("Global sensitivity of", deparse(substitute(calculate_km_estimate)),
+#             find_global_sensitivity(1, 4, 2, calculate_km_estimate)))
+
+# cat(paste("Global sensitivity of", deparse(substitute(calculate_hazard_ratio)),
+#           find_global_sensitivity(num_trt=2, N=4, max_aval=2, calculate_hazard_ratio)))
+
+cat(paste("Global sensitivity of", deparse(substitute(calculate_logrank_pvalue)),
+          find_global_sensitivity(num_trt=2, N=3, max_aval=4, calculate_logrank_pvalue)))
