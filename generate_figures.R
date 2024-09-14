97ca12cf6bfa830a5a9cc4b435df3cc469632039
fa-bula <fa_bula@mail.ru>
2024-09-14 23:34:44 +0200

    Added generate_figures.R

---
+++ generate_figures.R
@@ -0,0 +1,43 @@
+source("KM_estimate.R")
+library(DPpack)
+
+# Define the values for N, lambda, and p_cens
+N_values <- c(100)
+lambda_values <- c(0.3)
+p_cens_values <- c(0.3)
+eps_values <- seq(0.01, 1, by = 0.01)
+
+# Create a data frame with all combinations using expand.grid
+params <- expand.grid(N = N_values, lambda = lambda_values, p_cens = p_cens_values)
+
+# Function to apply function f to generated data.frame for each combination
+generate_and_apply <- function(N, lambda, p_cens, f, sensitivity) {
+  # Generate random survival times using exponential distribution
+  time <- rexp(N, rate = lambda)
+  
+  # Generate random event indicators
+  event <- rbinom(N, size = 1, prob = 1 - p_cens)
+  
+  # Combine survival times and event indicators into a data frame
+  df <- data.frame(
+    time = time,
+    event = event
+  )
+  
+  # Calculate value of a function
+  true_f_value = f(df)
+  
+  # Add laplace noise to value with different privacy budget
+  dp_f_values <- sapply(eps_values, function(eps) LaplaceMechanism(true.values = true_f_value,
+                                      eps = eps,
+                                      sensitivities = sensitivity))
+  
+  plot(eps_values, dp_f_values, main = "Scatter Plot Example", xlab = expression(epsilon), ylab = "Private KM estimate", col = "blue", pch = 16)
+  abline(h = true_f_value, col = "black", lty = 2)
+}
+
+# Apply the function to each row of the params data frame
+df_list <- apply(params, 1, function(x) 
+    generate_and_apply(x[1], x[2], x[3], calculate_km_estimate, 1 / x[1]))
+
+
