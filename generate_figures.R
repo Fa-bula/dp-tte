84013445b683bb6d1edcc42079697441f03752e9
fa-bula <fa_bula@mail.ru>
2024-09-22 18:16:38 +0200

    Added q1 and q3

--- generate_figures.R
+++ generate_figures.R
@@ -5,8 +5,8 @@ library(DPpack)
 N_values <- c(100, 200)
 lambda_values <- c(0.3)
 p_cens_values <- c(0.3)
-eps_values <- seq(0.01, 1, by = 0.01)
-K <- 10
+eps_values <- seq(0.01, 0.5, by = 0.025)
+K <- 100
 
 # Create a data frame with all combinations using expand.grid
 params <- expand.grid(N = N_values, lambda = lambda_values, p_cens = p_cens_values)
@@ -28,19 +28,28 @@ generate_and_apply <- function(N, lambda, p_cens, f, sensitivity) {
   # Calculate value of a function
   true_f_value = f(df)
   
-  dp_f_values <- numeric(length(eps_values))
+  # Calculate private f value K times
+  dp_f_values <- list()
   for (k in 1:K) {
     # Add laplace noise to value with different privacy budget
-    dp_f_values <- dp_f_values + sapply(eps_values, function(eps) LaplaceMechanism(true.values = true_f_value,
-                                                                     eps = eps,
-                                                                     sensitivities = sensitivity))
+    dp_f <- sapply(eps_values, function(eps) LaplaceMechanism(true.values = true_f_value,
+                                                              eps = eps,
+                                                              sensitivities = sensitivity))
+    dp_f_values <- append(dp_f_values, list(dp_f))
   }
-  dp_f_values <- dp_f_values / K
-  
-  plot(x = eps_values, y = dp_f_values, ylim=c(0, 1),
-       main = paste("Scatter Plot ", N), 
+  # Combine into matrix
+  dp_f_matrix <- do.call(rbind, dp_f_values)
+  q1_values <- apply(dp_f_matrix, 2, function(x) quantile(x, probs = 0.25))
+  mean_values <- apply(dp_f_matrix, 2, function(x) quantile(x, probs = 0.5))
+  q3_values <- apply(dp_f_matrix, 2, function(x) quantile(x, probs = 0.75))
+ 
+  cex = 0.6
+  plot(x = eps_values, y = mean_values, ylim=c(0.3, 1.3),
+       main = paste("Scatter Plot ", N),
        xlab = expression(epsilon), ylab = "Private KM estimate",
-       col = "blue", pch = 16)
+       col = "#377EB8", pch = 19, cex = cex)
+  points(x = eps_values, y = q1_values, col = "#E41A1C", pch = 19, cex = cex)
+  points(x = eps_values, y = q3_values, col = "#4DAF4A", pch = 19, cex = cex)
   abline(h = true_f_value, col = "black", lty = 2)
 }
 
